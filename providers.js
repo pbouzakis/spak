c2b6cde73e73cff8741bab88dcaf07458a0a1837
Paul Bouzakis <github@paulbouzakis.com>
2015-08-01 11:29:14 -0400

    Provide a custom entry point module for providers.
    Make it easy for prototypes and unit tests to get access and not have to path inside the package.

---
+++ providers.js
@@ -0,0 +1,8 @@
+import ProvidedAppDelegate from "./lib/ProvidedAppDelegate";
+import SimpleSession from "./lib/providers/SimpleSession";
+import ConsoleLogger from "./lib/providers/NullLogger";
+import NullLogger from "./lib/providers/NullLogger";
+import KeyWordLocalization from "./lib/providers/KeyWordLocalization";
+
+export { ProvidedAppDelegate,
+         SimpleSession, ConsoleLogger, NullLogger, KeyWordLocalization }
