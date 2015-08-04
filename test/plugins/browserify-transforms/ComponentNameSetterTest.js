a6cdd09884f0b23ad1960f03c71b3997c7752a77
Paul Bouzakis <github@paulbouzakis.com>
2015-08-04 18:19:43 -0400

    `ComponentNameSetter` also looks for actions.

--- test/plugins/browserify-transforms/ComponentNameSetterTest.js
+++ test/plugins/browserify-transforms/ComponentNameSetterTest.js
@@ -16,19 +16,69 @@ describe("ComponentNameSetter:", function () {
         this.annotator.should.respondTo("write");
     });
 
-    describe("when the filename matches a standard ViewUI filename", () => {
+    describe("when the filename matches a standard UIComponent filename", () => {
         beforeEach(() => {
+            this.writer.file = "node_modules/@app/foo/lib/ui/VeryAwesomeUI.js";
             this.writer.filename = "VeryAwesomeUI.js";
-            this.annotator.write();
         });
 
-        it ("should add a component name property to the module's prototype", () => {
-            this.writer.appendToDefaultExport.should.have.been.calledWithMatch("prototype.componentName = \"veryAwesome\"");
+        describe("and when the file contains an es default export", () => {
+            beforeEach(() => {
+                this.writer.body = "\nexport default class Foo {\n\n}\n";
+                this.annotator.write();
+            });
+
+            it("should add a component name property to the module's prototype", () => {
+                this.writer.appendToDefaultExport.should.have.been.calledWithMatch(
+                    "prototype.componentName = \"veryAwesome\""
+                );
+            });
+        });
+
+        describe("and when the file contains an node default export", () => {
+            beforeEach(() => {
+                this.writer.body = "\nmodule.exports = function Foo (){\n\n}\n";
+                this.annotator.write();
+            });
+
+            it("should add a component name property to the module's prototype", () => {
+                this.writer.appendToDefaultExport.should.have.been.calledWithMatch(
+                    "prototype.componentName = \"veryAwesome\""
+                );
+            });
+        });
+
+        describe("and when the file DOES NOT contain a default export", () => {
+            beforeEach(() => {
+                this.writer.body = "\nexport class Foo {\n\n}\nexport class Foo {\n\n}\n";
+                this.annotator.write();
+            });
+
+            it("should add a component name property to the module's prototype", () => {
+                this.writer.appendToDefaultExport.should.not.have.been.calledWithMatch(
+                    "prototype.componentName = \"veryAwesome\""
+                );
+            });
         });
     });
 
-    describe("when the filename does not match a standard ViewUI filename", () => {
-        it ("should not add any prototype property", () => {
+    describe("when the file is located inside an actions folder", () => {
+        beforeEach(() => {
+            this.writer.file = "node_modules/@app/foo/lib/actions/AddItem.js";
+            this.writer.filename = "AddItem.js";
+            this.writer.body = "\nexport default class AddItem {\n\n}\n";
+            this.annotator.write();
+        });
+
+        it("should add a component name property to the module's prototype", () => {
+            this.writer.appendToDefaultExport.should.have.been.calledWithMatch(
+                "prototype.componentName = \"addItem\""
+            );
+        });
+    });
+
+    describe("when the filename does not find a standard component", () => {
+        it("should not add any prototype property", () => {
             this.annotator.write();
             this.writer.appendToDefaultExport.should.not.have.been.called;
         });
