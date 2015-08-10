e9e898541a0fc3b298c9a510bcd8da939e7bceb6
Paul Bouzakis <github@paulbouzakis.com>
2015-08-10 08:58:44 -0400

    Add base `Action` class.
    Should be used as a base class for other actions to extend.
    Provides
      - ns property (used for logging and events)
      - app event bus

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import App from "./lib/App";
+import Action from "./lib/Action";
 import AppDelegate from "./lib/AppDelegate";
 
-export { App, AppDelegate };
+export { App, Action, AppDelegate };
